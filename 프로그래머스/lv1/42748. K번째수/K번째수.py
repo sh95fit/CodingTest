b4e79819e3fe21652117e5bdffdd9f1066f97754
sh95fit <110766687+sh95fit@users.noreply.github.com>
2022-11-12 21:36:36 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -3,8 +3,6 @@ def solution(array, commands):
     answer = []
     for i in range(len(commands)) :
         num.append(array[(commands[i][0]-1) : (commands[i][1])])
-    for i in range(len(num)) :
         num[i].sort()
-    for i in range(len(commands)) :
         answer.append(num[i][commands[i][2]-1])
     return answer
