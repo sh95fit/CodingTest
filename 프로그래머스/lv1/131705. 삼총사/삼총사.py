57d509fd8e0879b4fd1fcc5d9fd0b9aa05046102
sh95fit <110766687+sh95fit@users.noreply.github.com>
2022-11-25 21:41:51 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,5 +1,9 @@
+import itertools
 def solution(number):
     number = list(map(int,number))
+    result = 0
+    '''
+    # 1차 풀이
     sum = 0
     result = 0
     for i in range(len(number)) :
@@ -9,4 +13,9 @@ def solution(number):
                 if sum == 0 :
                     result += 1
                 sum = 0
+    '''      
+    # 2차 풀이
+    for i in itertools.combinations(number,3):
+        if sum(i) == 0 :
+            result += 1
     return result
