120eba2c9c1f5ed07df17ee863812139a3ac27e2
sh95fit <110766687+sh95fit@users.noreply.github.com>
2022-11-29 23:28:27 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,7 +1,12 @@
 def solution(my_string):
+    ''' 
     answer = []
     for i in range(len(my_string)) :
         if my_string[i].isdigit() :
             answer.append(int(my_string[i]))
+    answer.sort()
+    return answer
+    '''
+    answer = list(map(int, filter(lambda x : x.isdigit(), my_string)))
     answer.sort()
     return answer
