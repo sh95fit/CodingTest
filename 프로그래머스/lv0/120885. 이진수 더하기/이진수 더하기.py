07694cd638ff83d3562604bf633d16b0acc5daca
sh95fit <110766687+sh95fit@users.noreply.github.com>
2023-04-08 23:21:04 +0900

    [level 0] Title: 이진수 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,6 +1,6 @@
 def solution(bin1, bin2):
-    bin1 = '0b' + bin1
-    bin2 = '0b' + bin2
+    # bin1 = '0b' + bin1
+    # bin2 = '0b' + bin2
     result = bin(int(bin1,2)+int(bin2,2))[2:]
     # result = result.replace('0b', '')
     
