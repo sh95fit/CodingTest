cc2a13645b22856110740daa8f48425e430b39d6
sh95fit <110766687+sh95fit@users.noreply.github.com>
2023-09-28 16:06:35 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,11 @@
+def solution(myString, pat):
+    answer = ''
+    for i in myString :
+        if i == "A" :
+            answer += "B"
+        else :
+            answer += "A"
+    if pat in answer :
+        return 1
+    else :
+        return 0
