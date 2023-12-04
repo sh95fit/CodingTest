c0d7788fec7be6c4b1aa0abf5422c576ec6d9d01
sh95fit <110766687+sh95fit@users.noreply.github.com>
2023-12-04 17:07:13 +0900

    [level unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    for i in range(len(order)) :
+        if "americano" in order[i] :
+            answer += 4500
+        elif "cafelatte" in order[i] :
+            answer += 5000
+        elif "anything" in order[i] :
+            answer += 4500
+    return answer
