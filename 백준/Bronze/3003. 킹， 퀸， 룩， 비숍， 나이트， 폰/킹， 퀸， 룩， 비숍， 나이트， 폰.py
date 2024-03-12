36f26274211cd9e007f8e2a15252a645697d78a2
sh95fit <110766687+sh95fit@users.noreply.github.com>
2024-03-12 17:03:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,17 @@
+white_pieces = list(map(int, input().split()))
+
+correct_pieces = [1, 1, 2, 2, 2, 8]
+
+result = [correct - white for correct, white in zip(correct_pieces, white_pieces)]
+print(*result)
+
+#original = list(map(int, input().split()))
+#real = [1, 1, 2, 2, 2, 8]
+#res = []
+
+#for i, o in enumerate(original):
+#    if real[i] != o:
+#        res.append(real[i] - o)
+
+#print(*res)
+        
