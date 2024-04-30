4092799f571d9fb09d8f3c122ecd614d9b2d9e11
sh95fit <110766687+sh95fit@users.noreply.github.com>
2024-04-30 23:48:37 +0900

    [Silver IV] Title: 문자열 집합, Time: 924 ms, Memory: 36548 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+
+S = set()
+
+for _ in range(N):
+
+    S.add(input())
+
+count = 0
+
+for _ in range(M):
+
+    word = input()
+
+    if word in S:
+
+        count += 1
+
+print(count)
+
