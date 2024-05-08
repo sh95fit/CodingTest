8f48195dd173f3452bdb5828e65bb2f0eee75ede
sh95fit <110766687+sh95fit@users.noreply.github.com>
2024-05-08 15:48:42 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,11 @@
+n = int(input())  # 입력 크기 n을 받음
+
+# 코드1의 수행 횟수는 입력 크기 n과 동일함
+code1_executions = n
+
+# 코드1의 수행 횟수를 다항식으로 나타냈을 때 최고차항의 차수는 1임
+degree_of_polynomial = 1
+
+# 결과 출력
+print(code1_executions)
+print(degree_of_polynomial)
