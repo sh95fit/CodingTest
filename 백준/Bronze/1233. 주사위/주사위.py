56d56985eba6e94d0005f428795a9469a69b222a
sh95fit <110766687+sh95fit@users.noreply.github.com>
2024-10-06 21:29:25 +0900

    [Bronze II] Title: 주사위, Time: 56 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.py
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -1,25 +1,28 @@
-def most_frequent_sum(S1, S2, S3):
+def most_frequent_dice_sum(S1, S2, S3):
     from collections import defaultdict
+  
+    sum_count = defaultdict(int)
+       
+    for die1 in range(1, S1 + 1):
+        for die2 in range(1, S2 + 1):
+            for die3 in range(1, S3 + 1):
+                current_sum = die1 + die2 + die3
+                sum_count[current_sum] += 1
+       
+    max_frequency = -1
+    most_frequent_sum = None
+    
+    for sum_value, count in sum_count.items():
+        if count > max_frequency:
+            max_frequency = count
+            most_frequent_sum = sum_value
 
-    frequency = defaultdict(int)
-
-    for i in range(1, S1 + 1):
-        for j in range(1, S2 + 1):
-            for k in range(1, S3 + 1):
-                total_sum = i + j + k
-                frequency[total_sum] += 1
-
-    max_frequency = 0
-    most_frequent_sum = 0
-
-    for total_sum in sorted(frequency.keys()):
-        if frequency[total_sum] > max_frequency:
-            max_frequency = frequency[total_sum]
-            most_frequent_sum = total_sum
-
+        elif count == max_frequency:
+            most_frequent_sum = min(most_frequent_sum, sum_value)
+    
     return most_frequent_sum
 
 S1, S2, S3 = map(int, input().split())
 
-print(most_frequent_sum(S1, S2, S3))
+print(most_frequent_dice_sum(S1, S2, S3))
 
