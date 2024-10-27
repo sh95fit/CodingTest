cc69be5286ac54cf32da6ba1ad779d3b4c34c5eb
sh95fit <110766687+sh95fit@users.noreply.github.com>
2024-10-27 18:11:59 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -11,3 +11,32 @@ def evaluate(expression):
         return("O")
     else:
         return("X")
+
+# import ast
+# import operator
+
+# def solution(quiz):
+#     return [evaluate(expression) for expression in quiz]
+
+# def evaluate(expression):
+#     exp, ans = expression.split(" = ")
+#     calculated_value = evaluate_expression(exp.strip())
+    
+#     return "O" if calculated_value == int(ans.strip()) else "X"
+
+# def evaluate_expression(expr):
+#     node = ast.parse(expr, mode='eval')
+#     return eval_node(node.body)
+
+# def eval_node(node):
+#     if isinstance(node, ast.BinOp):
+#         return OPERATORS[type(node.op)](eval_node(node.left), eval_node(node.right))
+#     elif isinstance(node, ast.Num):
+#         return node.n
+#     else:
+#         raise ValueError("Unsupported operation")
+
+# OPERATORS = {
+#     ast.Add: operator.add,
+#     ast.Sub: operator.sub,
+# }
