996b897ce26ce9b2853b8547b1f2d473bf27f5ff
sh95fit <110766687+sh95fit@users.noreply.github.com>
2024-11-23 01:22:19 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 652 ms, Memory: 39664 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,29 @@
+memo = [[[None] * 101 for _ in range(101)] for _ in range(101)]
+
+def w(a, b, c):   
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+   
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+     
+    if memo[a][b][c] is not None:
+        return memo[a][b][c]
+       
+    if a < b and b < c:
+        result = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    else:
+        result = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+       
+    memo[a][b][c] = result
+
+    return result
+
+while True:
+    a, b, c = map(int, input().split())
+
+    if a == -1 and b == -1 and c == -1:
+        break
+ 
+    print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
+
