44689f99078766b222d0cbeee98226fed485c5dc
sh95fit <110766687+sh95fit@users.noreply.github.com>
2025-01-24 23:58:02 +0900

    [Silver IV] Title: 대칭 차집합, Time: 280 ms, Memory: 112820 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())  
+A = set(map(int, input().split()))  
+B = set(map(int, input().split()))  
+
+symmetric_difference = (A - B) | (B - A)
+
+print(len(symmetric_difference))
+
