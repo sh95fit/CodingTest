7bbb08f35e36d1e04a0e8e450ff3b4678b098b81
sh95fit <110766687+sh95fit@users.noreply.github.com>
2025-03-10 23:19:41 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,17 @@
+N = int(input())  
+
+people = [tuple(map(int, input().split())) for _ in range(N)]  
+ranks = []
+
+for i in range(N):
+    rank = 1  
+
+    for j in range(N):
+        if i != j:             
+            if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+                rank += 1  
+
+    ranks.append(rank)
+
+print(" ".join(map(str, ranks)))
+
