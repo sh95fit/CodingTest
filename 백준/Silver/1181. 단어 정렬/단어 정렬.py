ae935029d974eec132268421bb0716348fef892a
sh95fit <110766687+sh95fit@users.noreply.github.com>
2025-03-14 23:29:57 +0900

    [Silver V] Title: 단어 정렬, Time: 576 ms, Memory: 37532 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,12 @@
+N = int(input())  
+
+words = set()  
+
+for _ in range(N):
+    words.add(input().strip())
+
+sorted_words = sorted(words, key=lambda x: (len(x), x))
+
+for word in sorted_words:
+    print(word)
+
